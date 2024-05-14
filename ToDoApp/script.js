48e9f78f5b32ee5a7a5370f730253e8da34a4f8b
Nikhil <nikhilgovindraju@gmail.com>
2024-05-13 17:54:47 -0700

    Complete to-do app with localstorage to store the data!

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -10,9 +10,33 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML=inputBox.value;
         lisContainer.appendChild(li);
-        let span=document.createElement("span");
+        let span = document.createElement("span");
         span.innerHTML= "\u00d7";
         li.appendChild(span);
     }
     inputBox.value='';
+    saveData();
 }
+
+lisContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",lisContainer.innerHTML);
+
+}
+
+function getData(){
+    lisContainer.innerHTML=localStorage.getItem("data");
+
+}
+
+getData();
